80688980323ed56a35d46a7501313221c08a5d99
Khalmatov <large000@yandex.ru>
2021-11-23 22:18:54 +0300

    Выдает отправленные сообщение в из телеграмма в консоль

---
+++ Telegram_bot/BotCredentials.cs
@@ -0,0 +1,10 @@
+﻿namespace Telegram_bot
+{
+    internal static class BotCredentials
+    {
+        private static string botToken = "2134031347:AAHUa4ERojB-8-4g-t_eQ5UqHdwJHdQqXq4";
+
+        public static string BotToken { get;}
+
+    }
+}
--- Telegram_bot/Program.cs
+++ Telegram_bot/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using Telegram.Bot;
 
 namespace Telegram_bot
 {
     class Program
     {
+
+        private static string token { get; set; } = "2134031347:AAHUa4ERojB-8-4g-t_eQ5UqHdwJHdQqXq4";
+        private static TelegramBotClient botClient;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            botClient = new TelegramBotClient(token);
+            botClient.StartReceiving();
+            botClient.OnMessage += BotClient_OnMessage;
+            Console.ReadLine();
+            botClient.StopReceiving();
+            
+
+        }
+
+        private static async void BotClient_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            var me = await botClient.GetMeAsync();
+            Console.WriteLine(me.FirstName);
+            Console.WriteLine(e.Message.Text);
         }
     }
 }
