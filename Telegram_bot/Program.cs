e3f21beb5a0e9b76e63402074cf46149be3c66ed
Khalmatov <large000@yandex.ru>
2021-11-24 00:24:46 +0300

    Задание 11.3.7

--- Telegram_bot/Program.cs
+++ Telegram_bot/Program.cs
@@ -9,7 +9,19 @@ namespace Telegram_bot
 
         static void Main(string[] args)
         {
+            var bot = new BotWorker();
+            bot.Initialize(BotCredentials.BotToken);
+            bot.Start();
+            Console.WriteLine("Напишите stop для прекращения работы");
 
+
+            string command;
+            do
+            {
+                command = Console.ReadLine();
+            } while (command != "stop");
+
+            bot.Stop();
         }
 
 
